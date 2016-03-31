3ace5c999b2956ae8d01bbdedaa6dee40b64d896
zs <zs@126.com>
2016-03-31 12:00:11 +0800

    初始化项目

---
+++ MyDemo/MyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDemo
+//
+//  Created by sun on 16/3/31.
+//  Copyright © 2016年 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDemo/MyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDemo
+//
+//  Created by sun on 16/3/31.
+//  Copyright © 2016年 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
